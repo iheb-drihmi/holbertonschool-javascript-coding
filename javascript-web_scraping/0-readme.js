af4116f31318ee55c2952fe648e6904dd07ce0bf
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-09-22 13:41:54 +0100

    tasks

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', function (err, data) {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
